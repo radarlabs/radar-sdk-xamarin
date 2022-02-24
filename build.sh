04b4fa5879e57c2d6c7b5f7ea05cad77bdaaab0b
Cameron Morrow <cameron.morrow@toptal.com>
2022-02-24 13:11:51 -0500

    updated nuget version, modified build script

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
-﻿nuget restore
+﻿
+nuget restore
+
 find Samples -type f -name "*.csproj" | for proj in $(cat) ; do 
 msbuild $proj /p:Configuration=Release
-done
-find Samples -type f -name "*.csproj" | for proj in $(cat) ; do 
-msbuild $proj /p:Configuration=Debug
-done
+done || exit 1
+
+# find Samples -type f -name "*.csproj" | for proj in $(cat) ; do 
+# msbuild $proj /p:Configuration=Debug
+# done
