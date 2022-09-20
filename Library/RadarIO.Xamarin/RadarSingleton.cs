bcc77ad7a5c763890a8a847f62e8f328fc9d5a0a
Cameron Morrow <cameron.morrow@toptal.com>
2022-09-20 14:38:21 -0400

    minor changes

--- Library/RadarIO.Xamarin/RadarSingleton.cs
+++ Library/RadarIO.Xamarin/RadarSingleton.cs
@@ -8,9 +8,16 @@ namespace RadarIO.Xamarin
     public static class RadarSingleton
     {
         private static RadarSDK instance;
+        /// <summary>
+        /// Singleton instance of RadarSDK, provided for convenience. Must be initialized with a RadarSDKImpl instance before use.
+        /// </summary>
         public static RadarSDK Radar => instance
-            ?? throw new Exception("RadarSingleton must be initalized with a RadarSDKImpl instance before use.");
+            ?? throw new Exception("RadarSingleton must be initialized with a RadarSDKImpl instance before use.");
 
+        /// <summary>
+        /// Initialize the singleton with an instance of RadarSDKImpl.
+        /// </summary>
+        /// <param name="sdk">An instance of RadarSDKImpl.</param>
         public static void Initialize(RadarSDK sdk)
         {
             if (instance != null)
