39ed2f0bdab51be5239430d3efc98f33b40bf06f
Cameron Morrow <cameron.morrow@toptal.com>
2024-05-15 16:15:52 -0400

    fixing publishing

--- build.sh
+++ build.sh
@@ -6,3 +6,5 @@ dotnet build -c Release Library/RadarIO/RadarIO.csproj
 msbuild $proj /p:Configuration=Release Library/RadarIO.Xamarin.iOS/RadarIO.Xamarin.iOS.csproj
 msbuild $proj /p:Configuration=Release Library/RadarIO.Xamarin.Android/RadarIO.Xamarin.Android.csproj
 dotnet build -c Release Library/RadarIO.Maui/RadarIO.Maui.csproj
+
+nuget pack
