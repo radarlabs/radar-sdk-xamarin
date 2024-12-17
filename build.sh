881fb62003c9e69e84e450edfbdbcf8a4deb106c
Cameron Morrow <cameron.morrow@toptal.com>
2024-12-17 05:51:29 -0500

    removed xamarin projects

--- build.sh
+++ build.sh
@@ -3,6 +3,4 @@
 dotnet restore
 
 dotnet build -c Release Library/RadarIO/RadarIO.csproj
-#msbuild $proj /p:Configuration=Release Library/RadarIO.Xamarin.iOS/RadarIO.Xamarin.iOS.csproj
-#msbuild $proj /p:Configuration=Release Library/RadarIO.Xamarin.Android/RadarIO.Xamarin.Android.csproj
 dotnet build -c Release Library/RadarIO.Maui/RadarIO.Maui.csproj
